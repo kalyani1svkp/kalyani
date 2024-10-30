226e20874df429679b5e85f30b3a72c2f20bd1a3
Ashwaq <Ashwaq@DESKTOP-9C8LSB2>
2024-10-30 17:45:34 +0530

    first commit

---
+++ svkp_college/src/BasicOfJava/StringDemo.java
@@ -0,0 +1,10 @@
+package BasicOfJava;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+       String s1=("java string datatype");
+    		   System.out.println(s1);    		   
+	}
+
+}
