20ecdc6b9f12cdea56c4ca832a66db38ada16512
Ashwaq <Ashwaq@DESKTOP-9C8LSB2>
2024-10-30 16:30:09 +0530

    first commit

---
+++ svkp_college/src/BasicOfJava/HelloWorld.java
@@ -0,0 +1,11 @@
+package BasicOfJava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello worl");
+				System.out.println("kalyani");
+				System.out.println("java");
+	}
+
+}
