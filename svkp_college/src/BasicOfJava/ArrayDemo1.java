17870268eda6b31f02dfb5d3629fa1054352cf2a
Ashwaq <Ashwaq@DESKTOP-9C8LSB2>
2024-10-30 17:55:42 +0530

    first commit

---
+++ svkp_college/src/BasicOfJava/ArrayDemo1.java
@@ -0,0 +1,13 @@
+package BasicOfJava;
+
+public class ArrayDemo1 {
+
+	public static void main(String[] args) {
+        int num[] = {2,3,4};
+        System.out.println(num[0]);
+        System.out.println(num[2]);
+	
+
+	}
+
+}
