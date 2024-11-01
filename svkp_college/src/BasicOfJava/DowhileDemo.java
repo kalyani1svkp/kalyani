84c12558870c9fd5113126cb24535805b8927a37
Ashwaq <Ashwaq@DESKTOP-9C8LSB2>
2024-11-01 19:48:57 +0530

    first commit

---
+++ svkp_college/src/BasicOfJava/DowhileDemo.java
@@ -0,0 +1,15 @@
+package BasicOfJava;
+
+public class DowhileDemo {
+
+	public static void main(String[] args) {
+       int i=1;
+       do {
+    	   
+    	   System.out.println("Welcome the C2TC session");
+    	   i++;
+       
+       }while(i<=10);
+	}
+
+}
---
+++ svkp_college/src/BasicOfJava/EnhancedForEachLoop.java
@@ -0,0 +1,13 @@
+package BasicOfJava;
+
+public class EnhancedForEachLoop {
+
+	public static void main(String[] args) {
+     int num[]= {1,4,5,6,4,7,4,3,7};
+     for(int numbers:num)
+     {
+    	 System.out.println(numbers);
+     }
+	}
+
+}
