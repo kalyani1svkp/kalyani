99adc2a14cfe13c1513c814929759029ba1bb49e
Ashwaq <Ashwaq@DESKTOP-9C8LSB2>
2024-11-03 11:26:51 +0530

    first commit

---
+++ svkp_college/src/BasicOfJava/ArithmeticOperationDemo.java
@@ -0,0 +1,23 @@
+package BasicOfJava;
+
+public class ArithmeticOperationDemo {
+
+	public static void main(String[] args) {
+      int a=10, b=5;
+      int sum =a+b;
+      System.out.println("Addition:"+sum);
+      int diff=a-b;
+      System.out.println("Substraction:"+diff);
+      int product=a*b;
+      System.out.println("Multiplication:"+product);
+      int Quotient =a/b;
+      System.out.println("Division:"+ Quotient);
+      int remainder=a%b;
+      System.out.println("modulus:"+remainder);
+      
+      
+      
+      
+	}
+
+}
---
+++ svkp_college/src/BasicOfJava/SwitchDemo.java
@@ -0,0 +1,31 @@
+package BasicOfJava;
+
+public class SwitchDemo {
+
+	public static void main(String[] arsgs) {
+          String day="Monday";
+          switch(day)
+          {
+          case "Monday";
+          case "monday";
+          System.out.println("Attend the meetings");
+          break;
+          case  "Tuesday";
+          case  "tuesday";
+          break;
+          case "satursday";
+          case "satursday";
+          case "sunday";
+          case "sunday";
+          System.out.println("enjoy the weekend");
+          break;
+          default;
+          {
+        	  System.out.println("invail day";);
+        	  
+          }
+          }
+ 
+	}
+
+}
